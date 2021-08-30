5480401bddbbc9113fa5b9a71ecbb99529098108
nico <nicomtl193@gmail.com>
2021-08-30 16:08:26 -0300

    Add Case 2

--- MakingSense/Program.cs
+++ MakingSense/Program.cs
@@ -9,7 +9,7 @@ namespace MakingSense
         {
             Console.WriteLine("Hello World!");
             int opcion;
-            Console.WriteLine("Ingrese su opcion:\n1.Para Crear Auto");
+            Console.WriteLine("Ingrese su opcion:\n1.Para Crear Auto\n2 Para buscar por Id\n3");
             opcion = Convert.ToInt32(Console.ReadLine());
             Car car = new Car();
             CarCrud newCar = new CarCrud();
@@ -18,16 +18,16 @@ namespace MakingSense
             {
                 switch (opcion)
                 {
-                    case 1: newCar.Create(car);
+                    case 1: newCar.Create(1,"Fiat",4,"rojo","Manual");
                     break;
 
-                   case 2: newCar.Get(car.id);
+                    case 2: newCar.Get(1);
                     break;
                 }
 
 
 
-            } while (opcion != 1);
+            } while (opcion != 2);
 
 
 
