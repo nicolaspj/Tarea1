3e7bda4e3ebab4114fec89ce64b20762a0dcf898
nico <nicomtl193@gmail.com>
2021-08-31 20:09:06 -0300

    ingreso a do para evitar bucle en los cases

--- MakingSense/Program.cs
+++ MakingSense/Program.cs
@@ -7,15 +7,17 @@ namespace MakingSense
         
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           
             int opcion;
-            Console.WriteLine("Ingrese su opcion:\n1.Para Crear Auto\n2 Para buscar por Id\n3");
-            opcion = Convert.ToInt32(Console.ReadLine());
-            Car car = new Car();
-            CarCrud newCar = new CarCrud();
+           
 
             do
             {
+                Console.WriteLine("Ingrese su opcion:\n1.Para Crear Auto\n2 Para buscar por Id\n3");
+                opcion = Convert.ToInt32(Console.ReadLine());
+                Car car = new Car();
+                CarCrud newCar = new CarCrud();
+
                 switch (opcion)
                 {
                     case 1: newCar.Create(1,"Fiat",4,"rojo","Manual");
