60337d7a39660d4982d3f2d84fcca53f2f567701
nico <nicomtl193@gmail.com>
2021-09-01 19:32:39 -0300

    Creacion de Menu (case 1 y 2)

---
+++ MakingSense/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MakingSense
+{   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcion;
+            
+            Car car = new();
+            CarCrud newCar = new();
+            car.Id = 1;
+            car.CajaCambio = "Manual";
+            car.Color = "Rojo";
+            car.Modelo = "Fiat";
+            car.Puertas = 4;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Ingrese su opcion:\n1.Para Crear Auto\n2 Para buscar por Id\n3 Para Finalizar ");
+                opcion = Convert.ToInt32(Console.ReadLine());
+                switch (opcion)
+                {
+                    case 1:
+                        newCar.Create(car);
+                        break;
+
+                    case 2:
+                        newCar.Get(1);
+                        break;
+                }
+            } while (opcion != 3);
+
+        }
+
+    }
+}
